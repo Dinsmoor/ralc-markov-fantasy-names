63007720b9cfdf7116cb45de22ca3dc3d7127612
Tyler Dinsmoor <pappad@airmail.cc>
2015-08-20 23:01:17 +0900

    Modified for use with RALC.

--- nameGen.py
+++ nameGen.py
@@ -1,18 +1,18 @@
+# forked from https://github.com/AlexeyMK/markov-baby-names/blob/master/markovbaby.py
+
 import random
 
 from collections import defaultdict
-from sys import argv
 
 WORD_SEP = ' '
 
 class MarkovName:
-  def __init__(self, input_file):
+  def __init__(self, input_list):
     """ input file should have one name per line"""
-    markov_file = open(input_file, 'r')
     # markov chain is a dictionary from {(letter) to list-of-letters-seen-after}
     # {c: 'aaoehhhhh   '}
     self.chain = defaultdict(list)
-    names = (line for line in markov_file if not line[0] == '#')
+    names = (line for line in input_list if not line[0] == '#')
     for name in names:
       # Alice
       proper_name = name.lower().strip()
@@ -36,18 +36,10 @@ class MarkovName:
     return ''.join(name).strip().capitalize()
 
 
-if __name__ == '__main__':
-    try:
-      if len(argv) not in (2,3):
-        raise Exception("Too many or too few arguments")
-      names_file = argv[1]
-      num_names = 1 if len(argv) == 2 else int(argv[2])
-      chain = MarkovName(names_file)
-      for number in xrange(1, num_names + 1):
-        name = ''
-        while len(name) < 3 or len(name) > 15:
-          name = chain.generate_name()
-        print "Name #%d: %s" % (number, name)
-    except Exception as e:
-      print "Usage: python markovbaby.py input_file.txt [num_names]"
-      print "Exception: %s" % e
+def get_name(names_list):
+    chain = MarkovName(names_list)
+    name = ''
+    while len(name) < 3 or len(name) > 15:
+        name = chain.generate_name()
+    return name
+
